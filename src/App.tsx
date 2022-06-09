c28cd3b22c051d0a0af201045c35c2f61c56ad4a
Kleiton Marques <kleitonm@ciandt.com>
2022-06-09 19:28:41 -0300

    create admin room

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
 import { AuthContextProvider } from "./contexts/AuthContext";
+import { AdminRoom } from "./pages/AdminRoom";
 
 export function App() {
   
@@ -14,6 +15,7 @@ export function App() {
           <Route path="/" element={<Home />} />
           <Route path="/rooms/new" element={<NewRoom />} />
           <Route path="/rooms/:id" element={<Room />} />
+          <Route path="/admin/rooms/:id" element={<AdminRoom />} />
         </Routes>
       </AuthContextProvider>
     </BrowserRouter>
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -14,7 +14,7 @@ type RoomParams = {
     id: string;
 }
 
-export function Room () {
+export function AdminRoom () {
     const { user } = UseAuth()
     const params = useParams<RoomParams>();
     const [newQuestion, setNewQuestion] = useState('')
@@ -46,7 +46,10 @@ export function Room () {
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
-                    <RoomCode code={roomId || ''} />
+                    <div>
+                        <RoomCode code={roomId || ''} />
+                        <Button isOutlined>Encerrar sala</Button>
+                    </div>
                 </div>
 
                 <main>
@@ -54,25 +57,6 @@ export function Room () {
                         <h1>Sala {title}</h1>
                         {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
                     </div>
-
-                    <form onSubmit={handleSendQuastion}>
-                        <textarea
-                            placeholder='O que você quer perguntar?'
-                            onChange={event => setNewQuestion(event.target.value)}
-                            value={newQuestion}
-                        />
-
-                        <div className="form-footer">
-                            {user ? (
-                                <div className='user-info'>
-                                    {user?.avatar && <img src={user.avatar} alt={user.name} />}
-                                    <span>{user.name}</span>
-                                </div>
-                            ) : ( <span>Para enviar uma pergunta, <button>faça seu login</button>.</span> )}
-
-                            <Button type="submit" disabled={!user}>Enviar pergunta</Button>
-                        </div>
-                    </form>
 
                     <div className="question-list">
                         {questions.map(question => {
